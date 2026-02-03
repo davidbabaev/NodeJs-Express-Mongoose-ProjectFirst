7e915ee44d2da37f5c3d7c3dd251be2c7b80a07d
davidbabaev <davidbabaev198@gmail.com>
2026-02-03 15:28:57 +0200

    jwt and authService learning logics

--- src/auth/authService.js
+++ src/auth/authService.js
@@ -1,13 +1,25 @@
+const {verifyToken} = require('./providers/jwt')
+
 const auth = (req, res, next) => {
-    // 1) check it the header exists
-    if(req.header('x-auth-token')){
-        // 2) token exists --> let them through
-        console.log('user is authenticated');
-        next()
+    // 1) get the token from header
+    const token = req.header('x-auth-token');
+
+    // 2) check if token exists
+    if(!token){
+        return res.status(401).send('No token provided')
     }
-    // 3) no token --> block them
-    console.log('user is not authenticated');
-    res.status(401).send('User is not authorized')
+
+    // 3) verify the token is real
+    const userData = verifyToken(token);
+
+    // 4) check if verifications needed
+    if(!userData){
+        return res.status(401).send('Invalid token');
+    }
+
+    // 5) token i valid! attach user data to request
+    req.user = userData; // now routes can access req.user._id
+    next();
 }
 
 module.exports = auth;
--- src/auth/providers/jwt.js
+++ src/auth/providers/jwt.js
@@ -1,8 +1,31 @@
 const jwt = require('jsonwebtoken');
 
-const SECRET_WORD = 'secret';
+const SECRET_WORD = 'davidsecretword';
 
-const generateAuthToken = (user) => {};
+// called when user logs in - created a token
+const generateAuthToken = (user) => {
 
-const verifyToken = (tokenFromClient) => {};
+    // use jwt.sign() here
+    const token = jwt.sign(
+        {_id: user._id, isAdmin: user.isAdmin}, // --> payload - wht we store
+        SECRET_WORD // our secret stamp
+    );
 
+    return token;
+};
+
+const verifyToken = (tokenFromClient) => {
+    // important: jwt.verify() throws an error if token is invalid, so we need try/ catch
+    try{
+        // if token is valid, jwt.verify returns the payload
+        const payload = jwt.verify(tokenFromClient, SECRET_WORD);
+        return payload; // token in good
+    }
+    catch(err){
+        // if token is fake/ expired, jwt. verify throws error
+        // we catch it and return null
+        return null; // token is bad
+    }
+};
+
+module.exports = {generateAuthToken, verifyToken};
