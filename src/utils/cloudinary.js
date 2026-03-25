be4834e8b783114c4d04daa1b38f654a5f3254b4
davidbabaev <davidbabaev198@gmail.com>
2026-03-25 19:36:27 +0200

    Cloudinary file not saved code from the previous commit - the updated code

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,1 +1,24 @@
-// connects to Cloudinary and uploads the file, returns a URL.
+// v2 because cloudinary has two versions of their API, vs2 is the updated.
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+});
+
+const uploadToCloudinary = (fileBuffer, folder) => {
+    return new Promise((resolve, reject) => {
+        // create the stream, pass options + callback
+        cloudinary.uploader.upload_stream(
+            {folder: folder}, // store in this folder
+            (error, result)=> { // Cloudinary calls this when done
+                if(error) reject(error) // went wrong -> reject
+                    else resolve(result.secure_url) // success -> give back URL
+            }
+        ).end(fileBuffer) // push the buffer into the stream
+    })
+}
+
+module.exports = uploadToCloudinary;
