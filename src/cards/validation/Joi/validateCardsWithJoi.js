a73bb2f4cdef6aa1a469b6a912c2d6c5d4a69433
davidbabaev <davidbabaev198@gmail.com>
2026-03-29 18:06:49 +0300

    in usersRoutes - we removed from POST /users - createNewUser() - we removes the Cloudinary and multer logics that handle media uploading, because on wen user create new account we don't ask him to add any media, we ask for it after the user registered, he can edit his profile so on PUT we handle the logics of adding files, Profile picture and and profile cover image

--- src/cards/validation/Joi/validateCardsWithJoi.js
+++ src/cards/validation/Joi/validateCardsWithJoi.js
@@ -1,9 +1,9 @@
 const Joi = require('joi');
 
 const joiSchema = Joi.object({
-    title: Joi.string().min(3).max(256).required(),
+    title: Joi.string().max(256).allow('').optional(),
     content: Joi.string().min(3).max(1024).required(),
-    web: Joi.string().uri().min(3).max(1024),
+    web: Joi.string().uri().allow('').optional(),
     category: Joi.string().allow('').max(256),
     location: Joi.string().min(3).max(1024),
 });
--- src/users/routes/usersRoutes.js
+++ src/users/routes/usersRoutes.js
@@ -42,32 +42,12 @@ router.get('/users/:id', async (req, res) => {
     }
 })
 
-router.post('/users', uploadImageOnly.fields([
-        {name: 'profilePicture', maxCount: 1},
-        {name: 'coverImage', maxCount: 1}
-    ]) ,async (req, res) => {
+router.post('/users' ,async (req, res) => {
         try{
             const {error} = validateUser(req.body)
             if(error) return res.status(400).send(error.details[0].message);
-            
-            let profilePictureUrl;
-            let coverImageUrl;
-            
-            if(req.files['profilePicture']){
-                //file exists
-                profilePictureUrl = await uploadToCloudinary(req.files['profilePicture']?.[0].buffer, "users")
-            }
-            
-            if(req.files['coverImage']){
-                coverImageUrl = await uploadToCloudinary(req.files['coverImage']?.[0].buffer, "users")
-            }
-            
-            let newUser = await createNewUser({
-                ...req.body,
-                profilePicture: profilePictureUrl,
-                coverImage: coverImageUrl,
-            });
-
+        
+            let newUser = await createNewUser(req.body);
             res.send(newUser);
         }
         catch(err){
