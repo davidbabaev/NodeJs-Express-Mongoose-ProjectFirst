cda5e68800ef5e82f0cb61732a712fec0c62f899
davidbabaev <davidbabaev198@gmail.com>
2026-04-27 21:40:28 +0300

    Allow new Render frontend in CORS

--- src/app.js
+++ src/app.js
@@ -32,7 +32,8 @@ const io = new Server(server, {
         origin: [
             "http://localhost:5173",
             "http://localhost:8181",
-            "https://db-social-media-app.onrender.com"
+            "https://db-social-media-app.onrender.com",
+            "https://mirage-frontend-tfxf.onrender.com/"
         ]
     }
 })
